576b5666a74a86050f73785a91f72a7338cff574
Travis <NguyenTrav@gmail.com>
2020-02-10 20:13:29 -0500

    Updates

---
+++ precache-manifest.a244dded595494ee6e53494e3b5549e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d4f9ab92897335dad2389304db2910",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "e414cfa235d4648e755e",
+    "url": "/lights-out/static/css/main.cecdcae8.chunk.css"
+  },
+  {
+    "revision": "5fbbafdffa5088133732",
+    "url": "/lights-out/static/js/2.6c284c06.chunk.js"
+  },
+  {
+    "revision": "e414cfa235d4648e755e",
+    "url": "/lights-out/static/js/main.1fbffe59.chunk.js"
+  },
+  {
+    "revision": "c0d8557b2dc32e2a8d3a",
+    "url": "/lights-out/static/js/runtime-main.6173d6fd.js"
+  }
+]);
